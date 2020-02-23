3e48d7fcc23a6b6fab3e58b7d332808684283bbf
MondaleFelix <mondalefelix@yahoo.com>
2020-02-23 02:19:04 -0800

    added tab bar with two navigation controllers

---
+++ TakeHomeProject/TakeHomeProject/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  TakeHomeProject
+//
+//  Created by Mondale on 2/21/20.
+//  Copyright © 2020 Mondale. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+}
---
+++ TakeHomeProject/TakeHomeProject/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  TakeHomeProject
+//
+//  Created by Mondale on 2/21/20.
+//  Copyright © 2020 Mondale. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+}
